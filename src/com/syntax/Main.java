c1c917f5b75f20386c30b7504941e9f8da88d845
shiannea <shianne.ali99@gmail.com>
2022-05-14 11:00:19 -0400

    1st commit

---
+++ src/com/syntax/Main.java
@@ -0,0 +1,9 @@
+package com.syntax;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("creating a repo");
+        System.out.println("creating repo");
+    }
+}
